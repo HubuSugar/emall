a48803e0b9ea308589b2f37b02f7667b2f3c106b
HUXIAOGE <HUXIAOGE@KINGSOFT.COM>
2021-04-23 19:18:36 +0800

    网关配置

--- emall/mall-gateway/src/main/java/edu/hubu/mall/MallGateWayApplication.java
+++ emall/mall-gateway/src/main/java/edu/hubu/mall/MallGateWayApplication.java
@@ -1,5 +1,12 @@
 package edu.hubu.mall;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class MallGateWayApplication {
 
+    public static void main(String[] args) {
+        SpringApplication.run(MallGateWayApplication.class,args);
+    }
 }
