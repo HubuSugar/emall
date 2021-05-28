76311d258762c2fb6457e82cfa8ee26f5f643c7d
HUXIAOGE <HUXIAOGE@KINGSOFT.COM>
2021-05-28 21:24:18 +0800

    购物车业务

--- emall/mall-cart/src/main/java/edu/hubu/mall/cart/vo/Cart.java
+++ emall/mall-cart/src/main/java/edu/hubu/mall/cart/vo/Cart.java
@@ -1,6 +1,7 @@
 package edu.hubu.mall.cart.vo;
 
 import lombok.Data;
+import org.springframework.util.CollectionUtils;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -18,8 +19,83 @@ public class Cart {
     private List<CartItem> items;
 
     /**
-     * 总价格
+     * 商品的总数量
      */
-    private BigDecimal totalPrice;
+    private Integer countNum;
 
+    /**
+     * 商品的种类
+     */
+    private Integer countType;
+
+    /**
+     * 整个购物车总价格
+     */
+    private BigDecimal totalAmount;
+
+    /**
+     * 优惠价格
+     */
+    private BigDecimal reduce = new BigDecimal("0.00");
+
+    public List<CartItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<CartItem> items) {
+        this.items = items;
+    }
+
+    /**
+     * 累加每一个购物项的商品件数
+     * @return
+     */
+    public Integer getCountNum() {
+        Integer count = 0;
+        if(!CollectionUtils.isEmpty(this.items)){
+            for (CartItem item:items) {
+                count += item.getCount();
+            }
+        }
+        return count;
+    }
+
+    /**
+     * 商品的种类
+     * @return
+     */
+    public Integer getCountType() {
+        if(CollectionUtils.isEmpty(items)){
+            return 0;
+        }
+        return items.size();
+    }
+
+    /**
+     * 购物车的总价
+     * @return
+     */
+    public BigDecimal getTotalAmount() {
+        BigDecimal amount = new BigDecimal("0.00");
+        if(!CollectionUtils.isEmpty(items)){
+            for (CartItem item: items) {
+                BigDecimal totalPrice = item.getTotalPrice();
+                amount = amount.add(totalPrice);
+            }
+        }
+        //减去优惠的价格
+        return amount.subtract(reduce);
+    }
+
+    public void setTotalAmount(BigDecimal totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public BigDecimal getReduce() {
+        return reduce;
+    }
+
+    public void setReduce(BigDecimal reduce) {
+        this.reduce = reduce;
+    }
 }
