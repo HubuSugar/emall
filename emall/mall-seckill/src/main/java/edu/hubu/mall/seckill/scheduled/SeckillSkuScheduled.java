fcebafeaaffa016b564a4ad2454ff2117f1d231f
HUXIAOGE <HUXIAOGE@KINGSOFT.COM>
2021-07-11 21:18:36 +0800

    秒杀服务--定时任务

--- emall/mall-seckill/src/main/java/edu/hubu/mall/seckill/scheduled/SeckillSkuScheduled.java
+++ emall/mall-seckill/src/main/java/edu/hubu/mall/seckill/scheduled/SeckillSkuScheduled.java
@@ -4,11 +4,16 @@ import org.springframework.stereotype.Component;
 
 /**
  * @Description: 上架秒杀商品的定时任务
- *          定时任务
- *
- *
  * @Author: huxiaoge
  * @Date: 2021-07-11
+ *
+ * 实现定时任务不阻塞：
+ *      1、提交到自定义异步任务线程池、CompatableFuture + executor
+ *      2、使用spring boot的定时任务
+ *        @EnableScheduling (使用taskSchedulingProperties自动配置)、还是存在任务阻塞的bug
+ *      3、使用spring的异步任务
+ *        @EnableAsync 开启异步任务功能
+ *        @Async 需要异步执行的方法
  **/
 @Component
 public class SeckillSkuScheduled {
